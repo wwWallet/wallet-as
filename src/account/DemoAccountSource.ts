39588b534643c949c754e68509d354748569ec53
Yorgos <dr.george995@gmail.com>
2026-02-04 14:06:08 +0200

    demo mode, prefil test user creds on demo mode

--- src/account/DemoAccountSource.ts
+++ src/account/DemoAccountSource.ts
@@ -1,11 +1,15 @@
 import IAccountSource from '../interfaces/IAccountSource';
 import Account from "../account/Account";
+import config from '../config';
 
 export class DemoAccountSource implements IAccountSource {
-  private accounts = [
-    new Account("test", {}),
-    new Account("test2", {}),
-  ]
+  private accounts: Account[] = []
+
+  constructor() {
+    if (config.demoUsername) {
+      this.accounts.push(new Account(config.demoUsername, {}));
+    }
+  }
 
   public async getAccounts() {
     return Promise.resolve(this.accounts);
